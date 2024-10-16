962b5a2077e31de5df8a3b7582b4130fc711dd67
UPIN-0583 <quochuystg2004@gmail.com>
2024-10-16 14:37:54 +0700

    Add menu and fix main_activity

--- app/src/main/java/com/example/moviemate/MainActivity.java
+++ app/src/main/java/com/example/moviemate/MainActivity.java
@@ -1,20 +1,55 @@
 package com.example.moviemate;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+//import com.example.moviemate.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+//        // Set the default fragment to HomeFragment
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.container, new HomeFragment())
+//                    .commit();
+//        }
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                Fragment selectedFragment = null;
+//
+//                if (item.getItemId() == R.id.nav_home) {
+//                    selectedFragment = new HomeFragment();
+//                }
+//// else if bạn muốn thêm các Fragment khác sau này, bạn có thể thêm tiếp các điều kiện:
+//                else if (item.getItemId() == R.id.nav_ticket) {
+//                    selectedFragment = new TicketFragment();
+//                } else if (item.getItemId() == R.id.nav_movie) {
+//                    selectedFragment = new MovieFragment();
+//                } else if (item.getItemId() == R.id.nav_profile) {
+//                    selectedFragment = new ProfileFragment();
+//                }
+//
+//
+//                getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.container, selectedFragment)
+//                        .commit();
+//
+//                return true;
+//            }
+//        });
     }
 }
