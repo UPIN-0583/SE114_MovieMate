4d0d79be7084a6c8aa76c1ab88c9287084198a20
VTTcgn <tanthoivo857@gmail.com>
2024-10-19 21:45:16 +0700

    add homepage

--- app/src/main/java/com/example/moviemate/MainActivity.java
+++ app/src/main/java/com/example/moviemate/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-//import com.example.moviemate.HomeFragment;
+import com.example.moviemate.fragments.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,21 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-//        // Set the default fragment to HomeFragment
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .replace(R.id.container, new HomeFragment())
-//                    .commit();
-//        }
-//
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                Fragment selectedFragment = null;
-//
-//                if (item.getItemId() == R.id.nav_home) {
-//                    selectedFragment = new HomeFragment();
-//                }
+        // Set the default fragment to HomeFragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new HomeFragment())
+                    .commit();
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                if (item.getItemId() == R.id.nav_home) {
+                    selectedFragment = new HomeFragment();
+                }
 //// else if bạn muốn thêm các Fragment khác sau này, bạn có thể thêm tiếp các điều kiện:
 //                else if (item.getItemId() == R.id.nav_ticket) {
 //                    selectedFragment = new TicketFragment();
@@ -42,14 +42,14 @@ public class MainActivity extends AppCompatActivity {
 //                } else if (item.getItemId() == R.id.nav_profile) {
 //                    selectedFragment = new ProfileFragment();
 //                }
-//
-//
-//                getSupportFragmentManager().beginTransaction()
-//                        .replace(R.id.container, selectedFragment)
-//                        .commit();
-//
-//                return true;
-//            }
-//        });
+
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, selectedFragment)
+                        .commit();
+
+                return true;
+            }
+        });
     }
 }
