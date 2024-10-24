b68107b16678e3b6f35e242fa19f93e0e68d1b5c
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-10-24 12:38:42 +0700

    Add ForgotPasswordActivity

--- app/src/main/java/com/example/moviemate/activities/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/moviemate/activities/ForgotPasswordActivity.java
@@ -3,16 +3,18 @@ package com.example.moviemate.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.moviemate.R;
+import com.example.moviemate.utils.CustomDialog;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
@@ -28,24 +30,43 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         });
 
         TextView emailInput = findViewById(R.id.forgot_pass_email);
+
+        ImageButton backBtn = findViewById(R.id.forgot_password_back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         Button continueBtn = findViewById(R.id.forgot_pass_continue_btn);
         continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (emailInput.getText().toString().isEmpty()) {
-                    showAlertDialog();
+                    CustomDialog.showAlertDialog(ForgotPasswordActivity.this, R.drawable.ic_error, "Error", "Please enter your email address.", false);
                     return;
                 }
+
+                sendResetPasswordEmail(emailInput);
             }
         });
     }
 
-    private void showAlertDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setIcon(R.drawable.ic_error);
-        builder.setTitle("Error");
-        builder.setMessage("Please enter your email address.");
-        builder.setPositiveButton("OK", null);
-        builder.show();
+    private void sendResetPasswordEmail(TextView emailInput) {
+        Button continueButton = findViewById(R.id.forgot_pass_continue_btn);
+        continueButton.setEnabled(false); // Disable button to prevent multiple clicks
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.sendPasswordResetEmail(emailInput.getText().toString())
+                .addOnCompleteListener(task -> {
+                    continueButton.setEnabled(true); // Enable button
+
+                    if (task.isSuccessful()) {
+                        CustomDialog.showAlertDialog(ForgotPasswordActivity.this, R.drawable.ic_success, "Success", "Password reset email sent.", true);
+                    } else {
+                        CustomDialog.showAlertDialog(ForgotPasswordActivity.this, R.drawable.ic_error, "Error", "Failed to send password reset email.", false);
+                    }
+                });
     }
 }
