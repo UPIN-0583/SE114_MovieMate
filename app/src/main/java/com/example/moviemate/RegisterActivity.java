30e51f0b64ae0ad9e1fffc1f2ad47adcd11d9024
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-10-24 13:27:05 +0700

    Create UI RegisterActivity

--- app/src/main/java/com/example/moviemate/RegisterActivity.java
+++ app/src/main/java/com/example/moviemate/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,14 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.moviemate.utils.CustomDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText emailField, passwordField;
+    private EditText emailField, passwordField, confirmPasswordField;
     private Button registerButton;
-    private TextView loginLink;
     private FirebaseAuth mAuth;
 
     @Override
@@ -34,13 +35,17 @@ public class RegisterActivity extends AppCompatActivity {
         emailField = findViewById(R.id.register_email);
         passwordField = findViewById(R.id.register_password);
         registerButton = findViewById(R.id.register_button);
-        loginLink = findViewById(R.id.login_link);
+        confirmPasswordField = findViewById(R.id.register_confirm_password);
+        TextView loginLink = findViewById(R.id.login_link);
+        ImageButton backBtn = findViewById(R.id.register_back_btn);
 
         registerButton.setOnClickListener(view -> registerUser());
 
         loginLink.setOnClickListener(view -> {
-            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-            startActivity(intent);
+            finish();
+        });
+
+        backBtn.setOnClickListener(view -> {
             finish();
         });
     }
@@ -48,18 +53,27 @@ public class RegisterActivity extends AppCompatActivity {
     private void registerUser() {
         String email = emailField.getText().toString().trim();
         String password = passwordField.getText().toString().trim();
+        String confirmPassword = confirmPasswordField.getText().toString().trim();
 
         if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show();
+            CustomDialog.showAlertDialog(RegisterActivity.this, R.drawable.ic_error, "Error", "Please enter all details", false);
             return;
         }
 
+        if (password.compareTo(confirmPassword) != 0) {
+            CustomDialog.showAlertDialog(RegisterActivity.this, R.drawable.ic_error, "Error", "Passwords do not match", false);
+            return;
+        }
+
+        registerButton.setEnabled(false);
+
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
+                    registerButton.setEnabled(true);
                     if (task.isSuccessful()) {
-                        Toast.makeText(RegisterActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                        CustomDialog.showAlertDialog(RegisterActivity.this, R.drawable.ic_success, "Success", "Registration successful", true);
                     } else {
-                        Toast.makeText(RegisterActivity.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        CustomDialog.showAlertDialog(RegisterActivity.this, R.drawable.ic_error, "Error", task.getException().getMessage(), false);
                     }
                 });
     }
