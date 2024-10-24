55dc9db05f08b15822c01049e008b53e681a1717
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-10-24 12:37:59 +0700

    Add CustomDialog to display message

--- app/src/main/java/com/example/moviemate/utils/CustomDialog.java
+++ app/src/main/java/com/example/moviemate/utils/CustomDialog.java
@@ -1,9 +1,11 @@
 package com.example.moviemate.utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,13 +16,14 @@ import com.example.moviemate.R;
 import java.util.Objects;
 
 public class CustomDialog {
-    public static void showAlertDialog(Context context, int icon, String title, String message) {
+    public static void showAlertDialog(Context context, int icon, String title, String message, Boolean closeActivityOnOk) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         View view = LayoutInflater.from(context).inflate(R.layout.dialog_alert_layout, null);
 
         ImageView iconView = view.findViewById(R.id.dialog_icon);
         TextView titleView = view.findViewById(R.id.dialog_title);
         TextView messageView = view.findViewById(R.id.dialog_content);
+        Button okButton = view.findViewById(R.id.dialog_ok);
 
         iconView.setImageResource(icon);
         titleView.setText(title);
@@ -29,6 +32,14 @@ public class CustomDialog {
         builder.setView(view);
         AlertDialog dialog = builder.create();
         Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        okButton.setOnClickListener(v -> {
+            dialog.dismiss();
+            if (closeActivityOnOk) {
+                ((Activity) context).finish();
+            }
+        });
+
         dialog.show();
     }
 }
