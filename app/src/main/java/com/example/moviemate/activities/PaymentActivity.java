70c449f742190d36db629b804c795097839605b9
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-11-17 17:48:57 +0700

    Process payment status

--- app/src/main/java/com/example/moviemate/activities/PaymentActivity.java
+++ app/src/main/java/com/example/moviemate/activities/PaymentActivity.java
@@ -1,6 +1,8 @@
 package com.example.moviemate.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -9,6 +11,8 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,6 +20,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.moviemate.R;
 import com.example.moviemate.adapters.PaymentMethodListAdapter;
+import com.example.moviemate.api.PayOsApiService;
+import com.example.moviemate.api.objects.payos.CreatePayLink;
+import com.example.moviemate.api.objects.payos.CreatePayLinkResponse;
+import com.example.moviemate.api.objects.payos.SimpleMovieDescription;
 import com.example.moviemate.models.PaymentMethod;
 import com.example.moviemate.utils.CustomDialog;
 
@@ -26,13 +34,19 @@ import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class PaymentActivity extends AppCompatActivity {
-    private static final int PAYMENT_TIMEOUT = 900000; // 15 minutes
+    private static final int PAYMENT_TIMEOUT = 600000; // 10 minutes
 
     private Timer paymentTimer;
     private int paymentTimeLeft = PAYMENT_TIMEOUT;
     private PaymentMethodListAdapter paymentMethodListAdapter;
     private Integer basePrice = null;
+    private ActivityResultLauncher<Intent> launcher;
 
     // UI Components
     private ImageView backButton;
@@ -57,6 +71,24 @@ public class PaymentActivity extends AppCompatActivity {
         setupListeners();
         initPaymentMethods();
         startPaymentTimer();
+
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_CANCELED) return;
+
+                    Intent data = result.getData();
+                    if (data == null) return;
+
+                    String status = data.getStringExtra("status");
+                    if (status == null) return;
+                    if (status.equals("cancel")) {
+                        cancelPayment("Payment cancelled by user");
+                    } else if (status.equals("success")) {
+                        CustomDialog.showAlertDialog(this, R.drawable.ic_success, "Success", "Payment successful", false);
+                    }
+                }
+        );
     }
 
     private void setupLayout(){
@@ -150,9 +182,70 @@ public class PaymentActivity extends AppCompatActivity {
             return;
         }
 
-        // Perform payment here
-        CustomDialog.showAlertDialog(this, R.drawable.ic_success, "Success", "Payment successful", false);
+        if (paymentMethodListAdapter.getSelectedPaymentMethod().getType() == PaymentMethod.TYPE.QR_CODE) {
+            payWithQRCode();
+        }
+        else {
+            CustomDialog.showAlertDialog(this, R.drawable.ic_success, "Info", "Comming soon", false);
+        }
     }
+
+    private void payWithQRCode() {
+        final String CANCEL_URL = "https://moviemate.com/cancel";
+        final String RETURN_URL = "https://moviemate.com/success";
+
+        // IMPORTANT: Please remove this line in production
+        totalMoneyTextView.setText(formatMoney(2000));
+
+        payBtn.setEnabled(false); // Disable pay button to prevent multiple payments
+        int totalMoney = parseMoney(totalMoneyTextView.getText().toString());
+        if (totalMoney == 0) {
+            // Do something with 0d payment
+            return;
+        }
+
+        int quantity = (int) (seatTextView.getText().toString().chars().filter(ch -> ch == ',').count() + 1);
+        SimpleMovieDescription movieDescription = new SimpleMovieDescription(movieTitleTextView.getText().toString(), quantity, totalMoney);
+        long expiredAt = (System.currentTimeMillis() / 1000) + (PAYMENT_TIMEOUT / 1000);
+
+        CreatePayLink data = new CreatePayLink(13, totalMoney, "Movie ticket",
+                List.of(movieDescription), CANCEL_URL, RETURN_URL, expiredAt);
+
+        Dotenv dotenv = Dotenv.configure().directory("/assets").filename("env").load();
+        PayOsApiService.payOsApiService.createPayLink(dotenv.get("PAYOS_CLIENT_ID"), dotenv.get("PAYOS_API_KEY"), data).enqueue(new Callback<CreatePayLinkResponse>() {
+            @Override
+            public void onResponse(Call<CreatePayLinkResponse> call, Response<CreatePayLinkResponse> response) {
+                payBtn.setEnabled(true);
+
+                if (!response.isSuccessful()) {
+                    CustomDialog.showAlertDialog(PaymentActivity.this, R.drawable.ic_error, "Error", "Create payment link failed", false);
+                    return;
+                }
+
+                CreatePayLinkResponse dataResponse = response.body();
+                Log.d("[MovieMate] PaymentActivity", Objects.requireNonNull(dataResponse).toString());
+
+                if (dataResponse == null || !dataResponse.getCode().equals("00")) {
+                    CustomDialog.showAlertDialog(PaymentActivity.this, R.drawable.ic_error, "Error", "Create payment link failed (PayOS API failed)", false);
+                    return;
+                }
+
+                String paymentLink = dataResponse.getData().getCheckoutUrl();
+                Intent intent = new Intent(PaymentActivity.this, PayOsActivity.class);
+                intent.putExtra("checkoutUrl", paymentLink);
+                intent.putExtra("cancelUrl", CANCEL_URL);
+                intent.putExtra("successUrl", RETURN_URL);
+                launcher.launch(intent);
+            }
+
+            @Override
+            public void onFailure(Call<CreatePayLinkResponse> call, Throwable throwable) {
+                CustomDialog.showAlertDialog(PaymentActivity.this, R.drawable.ic_error, "Error", "Create payment link failed (Network error)", false);
+            }
+        });
+
+    }
+
     private void cancelPayment(String reason) {
         if (paymentTimer != null) {
             paymentTimer.cancel();
