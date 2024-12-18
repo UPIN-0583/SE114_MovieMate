c00a806f3f5d0c65ca9c57af00aeeb46c5c0cc2f
manhtq0508 <manhtq0508@gmail.com>
2024-12-18 16:22:25 +0700

    Cập nhật xử lý riêng cho từng role

--- app/src/main/java/com/example/moviemate/adapters/MoviePagerAdapter.java
+++ app/src/main/java/com/example/moviemate/adapters/MoviePagerAdapter.java
@@ -3,17 +3,29 @@ package com.example.moviemate.adapters;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.moviemate.fragments.AdminMovieListFragment;
 import com.example.moviemate.fragments.MovieListFragment;
 
 public class MoviePagerAdapter extends FragmentStateAdapter {
+    private boolean isAdmin;
 
-    public MoviePagerAdapter(@NonNull Fragment fragment) {
+    public MoviePagerAdapter(@NonNull Fragment fragment, boolean isAdmin) {
         super(fragment);
+        this.isAdmin = isAdmin;
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
+        if (isAdmin) {
+            if (position == 0) {
+                return AdminMovieListFragment.newInstance("Now Playing"); // Now Playing
+            } else {
+                return AdminMovieListFragment.newInstance("Coming Soon"); // Coming Soon
+            }
+        }
+
         if (position == 0) {
             return MovieListFragment.newInstance("Now Playing"); // Now Playing
         } else {
