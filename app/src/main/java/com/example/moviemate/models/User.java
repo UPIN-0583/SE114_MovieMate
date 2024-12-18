996829c9b9ad287b5481055797589c2ee7f271e4
manhtq0508 <manhtq0508@gmail.com>
2024-12-18 16:19:26 +0700

    Thêm trường role

--- app/src/main/java/com/example/moviemate/models/User.java
+++ app/src/main/java/com/example/moviemate/models/User.java
@@ -1,6 +1,8 @@
 package com.example.moviemate.models;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     public String name;
     public String phone;
     public String email;
