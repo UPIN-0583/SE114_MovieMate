1d7a0df55d3250e4c2161f2bc0c80d31044944c7
manhtq0508 <manhtq0508@gmail.com>
2024-12-20 16:48:20 +0700

    Khởi tạo cloudinary

--- app/src/main/java/com/example/moviemate/MovieMateApp.java
+++ app/src/main/java/com/example/moviemate/MovieMateApp.java
@@ -2,12 +2,25 @@ package com.example.moviemate;
 
 import android.app.Application;
 
+import com.cloudinary.android.MediaManager;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class MovieMateApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        Dotenv dotenv = Dotenv.configure().directory("/assets").filename("env").load();
+        HashMap<String, String> config = new HashMap<>();
+        config.put("cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+        config.put("api_key", dotenv.get("CLOUDINARY_API_KEY"));
+        config.put("api_secret", dotenv.get("CLOUDINARY_API_SECRET"));
+
+        MediaManager.init(this, config);
     }
 }
